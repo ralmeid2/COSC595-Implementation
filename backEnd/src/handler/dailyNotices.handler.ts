dd91151c3bc4751e716e88c5182031320ea45005
clairegrady123 <cgrady3@myune.edu.au>
2023-05-03 17:40:17 +1000

    refined dailyNotices APIs

--- backEnd/src/handler/dailyNotices.handler.ts
+++ backEnd/src/handler/dailyNotices.handler.ts
@@ -7,34 +7,11 @@ const dailyNoticesHandler = express.Router();
 let gameId: number;
 let currentPlayer: string
 
-dailyNoticesHandler.get("/:userId", validateSchema(getDailyNoticesByIdSchema), async (req: Request, res: Response) => {
-    console.log("/:userId")
-    const userId = req.params.userId
-    console.log("userId", userId)
-    try{
-        const userGames = await getDailyNoticesByUserId(userId);
-        return res.status(200).send(userGames);
-    }catch (err) {
-        return res.status(500).send(err);
-    }
-});
-
 dailyNoticesHandler.get("/", validateSchema(getDailyNoticesSchema), async (req: Request, res: Response) => {
     const userId = req.userId
     try{
         const dn = await getAllDailyNotices();
         return res.status(200).send(dn);
-    }catch (err) {
-        return res.status(500).send(err);
-    }
-});
-
-dailyNoticesHandler.get("/:userId/:gameId", validateSchema(getDailyNoticesByIdSchema), async (req: Request, res: Response) => {
-    const g_Id = req.params.gameId
-    const userId = req.params.userId
-    try{
-        const oneGame = await getOne({ gameId: { $eq: g_Id }, userId: { $eq: userId }} );
-        return res.status(200).send(oneGame);
     }catch (err) {
         return res.status(500).send(err);
     }
@@ -47,6 +24,36 @@ dailyNoticesHandler.post("/", validateSchema(createDailyNoticesSchema), async (r
         return res.status(200).send(newGame)
      
  })
+
+
+
+
+
+
+dailyNoticesHandler.get("/:userId", validateSchema(getDailyNoticesByIdSchema), async (req: Request, res: Response) => {
+    console.log("/:userId")
+    const userId = req.params.userId
+    console.log("userId", userId)
+    try{
+        const userGames = await getDailyNoticesByUserId(userId);
+        return res.status(200).send(userGames);
+    }catch (err) {
+        return res.status(500).send(err);
+    }
+});
+
+
+dailyNoticesHandler.get("/:userId/:gameId", validateSchema(getDailyNoticesByIdSchema), async (req: Request, res: Response) => {
+    const g_Id = req.params.gameId
+    const userId = req.params.userId
+    try{
+        const oneGame = await getOne({ gameId: { $eq: g_Id }, userId: { $eq: userId }} );
+        return res.status(200).send(oneGame);
+    }catch (err) {
+        return res.status(500).send(err);
+    }
+});
+
 
 
  dailyNoticesHandler.delete("/:userId/:gameId", validateSchema(updateDailyNoticesSchema), async (req: Request, res: Response) => {
