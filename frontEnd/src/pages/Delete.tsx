d500aca934b468dbdcefcb5823d75f14149945b3
ralmeid2 <ralmeid2@myune.edu.au>
2023-05-21 12:06:55 +0800

    change delete visuals

--- frontEnd/src/pages/Delete.tsx
+++ frontEnd/src/pages/Delete.tsx
@@ -1,30 +1,23 @@
-import style from './Edit.module.css'
 import { useState, useCallback, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, Input, Message, AdminMenu } from '../components'
 import { del, get } from '../utils/http'
 import { DailyNotice } from '../types/DailyNotice'
-
-
+import styles from './Delete.module.css'
 
 export default function Delete() {
     const [dailyNotices, setDailyNotices] = useState<DailyNotice[]>([])
-
-
 
     useEffect(() => {
         fetchDailyNotices()
     }, [])
 
-
-    const radioHandler = (id: string) => {
+    const deleteHandler = (id: string) => {
         const confirm = window.confirm("Are you sure you want to delete this notice?")
         if (confirm === true) {
             deleteOneDailyNotice(id)
         }
-
     };
-
 
     const fetchDailyNotices = useCallback(async () => {
         try {
@@ -46,26 +39,23 @@ export default function Delete() {
         }
     }
 
-
     return (
-        <body>
-            <AdminMenu />
-            <div className={style.checkList}>
-                {dailyNotices.map(
-                    ({ _id, title, message, startDate, expiryDate }) => {
-                        return (
-                            <div className={style.item} key={_id}>
-                                <input value={_id} type="radio" id={_id} onChange={() => radioHandler(_id)} />
-
-                                {title} {message} {startDate} {expiryDate}
-                            </div>
-                        )
-                    }
-
-                )}
-
-            </div>
-        </body>
+      <body>
+      <AdminMenu />
+      <div className={styles.list} >
+      <h2>Please click on an item to delete it</h2>
+          {dailyNotices.map(
+            ({ _id, title, message, startDate, expiryDate }) => {
+                return (
+                  <div className={styles.item} key={_id} onClick={() => deleteHandler(_id)}>
+                                <span >
+                                    {title} {message} {startDate} {expiryDate}
+                                </span>
+                  </div>
+                )
+            }
+          )}
+      </div>
+      </body>
     )
-
 }
