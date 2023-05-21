1d362b5398c56501f7911984283bd18bf3445e9c
ralmeid2 <ralmeid2@myune.edu.au>
2023-05-21 13:50:02 +0800

    weather prettier and uses react-icons

--- frontEnd/src/components/Weather.tsx
+++ frontEnd/src/components/Weather.tsx
@@ -1,10 +1,72 @@
 import React, { useEffect, useState } from 'react';
+
+// Icon names from https://react-icons.github.io/react-icons/
+import {
+    FaSun,
+    FaMoon,
+    FaCloud,
+    FaSmog,
+    FaCloudRain,
+    FaSnowflake,
+    FaBolt,
+    FaSmile,
+    FaCloudSun,
+    FaCloudMoon
+} from "react-icons/fa";
+
 import styles from './Weather.module.css';
 
 export default function Weather({ weatherData, isLoading }: WeatherProps) {
     const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true}));
 
+    const weatherCodeToIcon = (code: number) => {
+        /*
+        Based on https://open-meteo.com/en/docs
+        0	Clear sky
+        1, 2, 3	Mainly clear, partly cloudy, and overcast
+        45, 48	Fog and depositing rime fog
+        51, 53, 55	Drizzle: Light, moderate, and dense intensity
+        56, 57	Freezing Drizzle: Light and dense intensity
+        61, 63, 65	Rain: Slight, moderate and heavy intensity
+        66, 67	Freezing Rain: Light and heavy intensity
+        71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+        77	Snow grains
+        80, 81, 82	Rain showers: Slight, moderate, and violent
+        85, 86	Snow showers slight and heavy
+        95 *	Thunderstorm: Slight or moderate
+        96, 99 *	Thunderstorm with slight and heavy hail
+         */
+
+        const currentHour = new Date().getHours();
+
+        const isDayTime = currentHour >= 6 && currentHour < 18;
+        const baseIcon = isDayTime ? <FaSun /> : <FaMoon />;
+
+        // Weird switch statement used to include multiple cases, see https://stackoverflow.com/a/60048899
+        switch (true) {
+            case code === 0:
+                return baseIcon;
+            case [1, 2].includes(code):
+                return isDayTime ? <FaCloudSun /> : <FaCloudMoon />;
+            case [3].includes(code):
+                return <FaCloud />;
+            case [45, 48].includes(code):
+                return <FaSmog />;
+            case [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(code):
+                return <FaCloudRain />;
+            case [71, 73, 75, 77, 85, 86].includes(code):
+                return <FaSnowflake />;
+            case [95, 96, 99].includes(code):
+                return <FaBolt />;
+            default:
+                // Smiley face if we don't have an icon for the weather code
+                return <FaSmile />;
+        }
+    }
+
+
     useEffect(() => {
+        // Getting time every second in HH:MM AM/PM format
         const timer = setInterval(() => {
             setCurrentTime(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true}));
         }, 1000);
@@ -19,9 +81,7 @@ export default function Weather({ weatherData, isLoading }: WeatherProps) {
             <div className={styles.weatherTitle}>Current Weather</div>
             <div className={styles.time}>{currentTime}</div>
             <div className={styles.weatherInfoContainer}>
-                <div className={styles.weatherIcon}>Code {weatherData.weathercode}</div>
-                {/*I'll leave this commented out while we implement the code -> icon logic*/}
-                {/*<img src={weatherData.weathercode} alt="weather icon" className={styles.weatherIcon} />*/}
+                <div className={styles.weatherIcon}>{weatherCodeToIcon(weatherData.weathercode)}</div>
                 <div className={styles.temperature}>{weatherData.current_temp}<sup className={styles.superscript}>°C</sup></div>
             </div>
         </div>
