4b53728fc1fb9126b3c686a94de879f1b75c632b
Benjamin Allan Burton <bburto23@myune.edu.au>
2023-05-29 17:59:29 +1000

    events handler doc and comments

--- backEnd/src/handler/events.handler.ts
+++ backEnd/src/handler/events.handler.ts
@@ -1,9 +1,32 @@
+
 //webcal://sec.act.edu.au/?post_type=tribe_events&tribe-bar-date=2023-05-27&ical=1&eventDisplay=list
 
 import express, { Request, Response, Router } from 'express';
-
 const eventsHandler: Router = express.Router();
 
+/*
+    Route handler for returning events from the school's publically available calendar. 
+    The calendar is retrieved from:
+    https://sec.act.edu.au/?post_type=tribe_events&tribe-bar-date=2023-05-27&ical=1&eventDisplay=list
+
+    The route is /api/events/
+
+    which returns a .ics universal calendar file format in the text field of the response. 
+
+    We use the ICAL library to parse this data, see:
+
+    https://www.npmjs.com/package/ical
+
+    The 5 closest upcoming events are returned in JSON format in the following format:
+    {
+        events: [
+            {
+                "event": string (the title of the event)
+                "date": the date of the event in the format "Mmm DD" e.g. "Mar 21"  
+            }
+        ]
+    }
+*/
 
 eventsHandler.get('/', async (req: Request, res: Response) => {
     const webcalLink = 'https://sec.act.edu.au/?post_type=tribe_events&tribe-bar-date=2023-05-27&ical=1&eventDisplay=list';
@@ -38,27 +61,25 @@ eventsHandler.get('/', async (req: Request, res: Response) => {
         "Sunday"
     ]
     let list = []
+    // parse event data and convert into correct format
     for (let i = 0; i < comp.jCal[2].length; i++) {
         if (comp.jCal[2][i][1][6] != undefined) 
         {
             const listDate = new Date(comp.jCal[2][i][1][0][3])
             if (!comp.jCal[2][i][1][6][3].startsWith("Week")){
                 list.push({
-                    event:comp.jCal[2][i][1][6][3],
+                    event: comp.jCal[2][i][1][6][3],
                     date: `${months[listDate.getMonth()]} ${listDate.getDate()}` 
                 })
             }
             
         }
     }
+    // only return the first 5 events
     list = list.slice(0,5)
-
 
     // Send the events as JSON in the response
     res.json({events:list});
-
 });
-
-
 
 export default eventsHandler;
