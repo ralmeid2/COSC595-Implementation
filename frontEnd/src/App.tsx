5ac228f1e930f48bcdc2b3fc813fa587b8902881
Benjamin Allan Burton <bburto23@myune.edu.au>
2023-08-31 15:17:33 +1000

    remove user provider, and UserProvider wrapper in App.tsx

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home, Login, SignUp, Add, Edit, Delete, Admin, Display, Photo, AdminOptions, OptionDisplay } from './pages'
-import { UserProvider } from './components'
 import './App.css';
 
 function App() {
   return (
-    <UserProvider>
     <main className="main">
       <Routes>
         <Route path="/" element={ <Home /> }/>
@@ -22,7 +20,6 @@ function App() {
         <Route path="fullscreen/:pageId" element={<OptionDisplay />}/>
       </Routes>
     </main>
-    </UserProvider>
   );
 }
 
--- frontEnd/src/components/index.ts
+++ frontEnd/src/components/index.ts
@@ -1,5 +1,4 @@
 
-export { default as UserProvider } from './UserProvider'
 export { default as Input } from './Input'
 export { default as Button } from './Button'
 export { default as Options } from './Options'
