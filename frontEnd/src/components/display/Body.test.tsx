b80610ec122b51a6b0c0bd74a90fa52461fcf9ff
Renan Massari <renanamass@gmail.com>
2023-10-02 11:37:39 +0800

    Add more tests to Body

---
+++ frontEnd/src/components/display/Body.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {fetchResponse, suppressConsole} from "../../testing";
+import Body from './Body';
+
+suppressConsole();
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+) as unknown as typeof fetch;
+
+beforeEach(() => {
+  (fetch as jest.Mock).mockClear();
+});
+
+describe('<Body />', () => {
+  it('should render loading state initially', async () => {
+    render(<Body />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('should fetch notices data on mount', async () => {
+    render(<Body />);
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalledWith('/api/dailyNotices');
+    });
+  });
+
+
+});
